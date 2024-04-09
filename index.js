7e3ce50fa60edc6cb2ff37378d4e43f95120e220
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-04-09 10:14:36 +0500

    Routes are nicelu mananged

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import googleAuth from "./routes/googleAuth.routes.js";
-import forgetPassword from "./routes/forgetPassword.js";
 
-import UserRoutes from "./routes/user.routes.js";
-import GenreRoutes from "./routes/genres.routes.js";
-import SeriesRoutes from "./routes/series.routes.js";
-import SeasonRoutes from "./routes/season.routes.js";
-import EpisodeRoutes from "./routes/episode.routes.js";
-import StreamRoutes from "./routes/streams.routes.js";
-import FileRoutes from "./routes/file.routes.js";
-
-
+import { unProtectedRouter } from "./routes/index.js";
 const app = express();
+app.use("/", unProtectedRouter);
+
 app.use(express.json());
 app.use(express.urlencoded());
-app.use("/users",UserRoutes);
-app.use("/genres",GenreRoutes);
-app.use("/series",SeriesRoutes);
-app.use("/seasons",SeasonRoutes);
-app.use("/episodes",EpisodeRoutes);
-app.use("/streams",StreamRoutes);
-app.use("/files",FileRoutes);
 
-
-app.use("/password",forgetPassword);
-app.use("/googleAuth",googleAuth);
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI,{
@@ -35,8 +16,7 @@ mongoose.connect(process.env.MONGO_URI,{
     useUnifiedTopology: true,
 });
 
-// const port = process.env.PORT || 3000; 
-const port = 2024 ;
+const port = process.env.PORT || 3000; 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
